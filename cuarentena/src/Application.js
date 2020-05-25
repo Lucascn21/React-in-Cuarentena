71f71f985455986eb3f81e42431c82212a87e4c8
lucascn21 <lucascn21@gmail.com>
2020-05-25 12:43:19 -0300

    Estados en react, metodos componentDid..

--- cuarentena/src/Application.js
+++ cuarentena/src/Application.js
@@ -1,54 +1,63 @@
 import React, { Component } from 'react';
 
 class Application extends Component {
-    //Metodo constructor, recibe un argumento props
-    constructor(props){
-        super(props);
-    }
-
-        componentWillMount(props, state){
-
-
-        }
-
-      
-
-
-        componentWillReceiveProps(props){
-
-        }
-
-        componentWillUpdate(props, state){
-            if (this.props.name!==props.name) {
-                //hacer algo
-            }
-        }
+//Algunos metodos de la clase App.
+/*
         //Esta es la manera mas apropiada, los will estan / seran deprecados
         componentDidUpdate(props, state){
-            
         }
-
         componentDidMount(props, state){
             console.log("Mounted with", props, state);
         }
+*/
 
 
+    //Metodo constructor, recibe un argumento props
+    constructor(props){
+        super(props);
 
+       this.state={
+           count:0,
+           overTen: false
+       }
+    }
+
+    handleClick =()=>{
+     //   console.log("clicked");
+        this.setState({count:this.state.count+1});
+    }
+    //Cada vez que clickeamos el boton, se llama a esta funcion, logueamos el estado previo y el estado actual.
+    componentDidUpdate(props, state){
+        console.log("Updated from", state, "to", this.state);
+        //Necesito la segunda parte de la condicion para evitar un loop infinito al usar setState para actualizar el componente, ademas verificamos que overTen sea falso para no estar saliendo por el true innecesariamente despues de las 10 veces
+        if(this.state.count>10 && this.state.count!==state.count && !this.state.overTen){
+            console.log("Updated over 10");
+            this.setState({overTen:true});
+
+        }
+    }
 
     //render retorna el html
     render() {
         //podemos usar y retornar variables en render.abs
         let name='Pepe';
+        let {count}=this.state;
+
 
 
 
 
        //Podemos retornar texto o en HTML. 
        // return 'Hello world';
+
+       //En el boton, capturamos el evento y lo mandamos al metodo que hicimos arriba
+       //Usamos un operador ternario para mostrar cuando se superen los 10 clicks
        return (
         <div> 
-       <h1>Hello {name} / World</h1>
-       <span>This</span>
+       <h1>Clickeaste el boton {count} veces</h1>
+       {(this.state.overTen)?<h3>Beat highscore of 10!</h3>:null}
+      
+       <span><button onClick={()=>this.handleClick()}>Clickeame!</button></span>
        <p>{name}</p>
        </div>  
        );
